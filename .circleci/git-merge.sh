279716d683d19fa4e39a5ceeac680ae2a946b9a1
Lars Kruse <lakruzz@praqma.com>
2018-03-31 21:02:08 +0200

    Circle config

--- .circleci/git-merge.sh
+++ .circleci/git-merge.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+set -e
+
+echo git rebase origin/master
+git rebase origin/master
+
+echo git checkout master
+git checkout master
+
+echo git pull
+git pull
+
 echo  git merge --ff-only $CIRCLE_BRANCH
 git merge --ff-only $CIRCLE_BRANCH
