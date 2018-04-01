30ca0f00c901c48a1feffd5575e200fbdf9dac01
Circle CI by @Lakruzz <circleci@lakruzz.com>
2018-04-01 14:48:46 +0200

    deliver

--- .circleci/deliver.sh
+++ .circleci/deliver.sh
@@ -9,10 +9,10 @@ GITREPO=`git remote get-url  origin | sed  s/'.*github.com[\/:]//' | sed s/.git/
 git config user.name "Circle CI by @Lakruzz"
 git config user.email "circleci@lakruzz.com"
 #git config --global push.default simple
-echo git remote add ghtoken https://$(printenv GHTOKEN)@github.com/$(printenv GITREPO).git
+git remote add ghtoken https://$(printenv GHTOKEN)@github.com/$(printenv GITREPO).git
 
 $SCRIPTDIR/integrate.sh
 
 echo $ git push
-[ ! -z "$DRYRUN" ] ||  git push github
+[ ! -z "$DRYRUN" ] ||  git push ghtoken
 git rev-parse --short HEAD
