8dfb29ffee99f60ec465236c5d9c498abdc27c54
Circle CI by @Lakruzz <circleci@lakruzz.com>
2018-04-01 14:39:22 +0200

    Ready for delivery

--- .circleci/deliver.sh
+++ .circleci/deliver.sh
@@ -4,9 +4,15 @@
 set -e
 
 SCRIPTDIR=`dirname $0`
+GITREPO=`git remote get-url  origin | sed  s/'.*github.com[\/:]//' | sed s/.git//`
+
+git config user.name "Circle CI by @Lakruzz"
+git config user.email "circleci@lakruzz.com"
+git config --global push.default simple
+echo git remote add ghtoken https://$(printenv GHTOKEN)@github.com/$(printenv GITREPO).git
 
 $SCRIPTDIR/integrate.sh
 
 echo $ git push
-[ ! -z "$DRYRUN" ] ||  git push
+[ ! -z "$DRYRUN" ] ||  git push github
 git rev-parse --short HEAD
